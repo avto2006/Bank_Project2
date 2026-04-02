5be598c8b21cd65395d5a57e9529b683a122976b
Avto Bagaturia <Avtobagaturia21@gmail.com>
2026-04-02 10:46:41 +0400

    new changes in structure, style, add responsive design (tablet and mobile) add logic

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 const registrerBox = document.querySelector('.registrer_box');
 const iconArea =  document.querySelector('.icon_area');
+const namee = document.querySelector('.name')
+const email = document.querySelector('.email')
 
 const func1 = () => {
         registrerBox.style.display = 'grid';
         iconArea.style.display = 'none';
 }    
+
+
+const func2 = () => {
+        if((namee.value !== '' && email.value !== '') 
+          && (email.value !== '' && namee.value !== '' )) {
+             alert("Application sent successfully.")
+        } else {
+           alert("Please fill in at least the name and email fields.")
+        }
+}
